1b17d7bfcdef1d411c31e5e344a48c25ff62ea47
jdbbaugh <jbaugh78@msn.com>
2018-12-19 12:56:00 -0600

    product modules are up and running, on to review modules

---
+++ src/scripts/product.js
@@ -0,0 +1,36 @@
+import productList from "./productList"
+
+const product = {
+  productBuilder (productObj) {
+    console.log(productObj)
+    let productArticle = document.createElement("article");
+    productArticle.setAttribute("id", `product-${productObj.id}`)
+    
+    let productImage = document.createElement("img");
+    productImage.setAttribute("src", productObj.image);
+    productImage.setAttribute("class", "image");
+
+    let productTitle = document.createElement("h3");
+    productTitle.textContent = productObj.title;
+
+    let productDescription = document.createElement("p");
+    productDescription.textContent = productObj.description;
+
+    let productPrice = document.createElement("p");
+    productPrice.textContent = `Price: ${productObj.price}`;
+
+    let productQuantity = document.createElement("p");
+    productQuantity.textContent = `Available: ${productObj.quantity}`;
+
+    productArticle.appendChild(productImage);
+    productArticle.appendChild(productTitle);
+    productArticle.appendChild(productPrice);
+    productArticle.appendChild(productQuantity);
+    productArticle.appendChild(productDescription);
+    
+
+    return productArticle;
+  }
+}
+
+export default product
